8e1fa5c457c7c68d4b660a514448e581c61f6364
nevelson <nevelson92@gmail.com>
2020-05-25 19:55:08 -0400

    Prepare for deploy

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  PORT: process.env.PORT,
+  PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV === "production" ? "tiny" : "common",
   API_TOKEN: process.env.API_TOKEN,
   JWT_TOKEN: process.env.JWT_TOKEN
